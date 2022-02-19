eead76af6c60a4d3d6728ad1719ecdfbb86f4162
Negar Pourmoazemi <80150087+negarpm@users.noreply.github.com>
2022-02-19 21:42:40 +0000

    Update mnist_mlp.py

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -9,12 +9,9 @@ import time
 # loading the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train_shape = x_train.shape
-x_test_shape = x_test.shape
-
 # reshaping the dataset
-x_train = np.reshape(x_train, (x_train_shape[0], x_train_shape[1] * x_train_shape[2]))
-x_test = np.reshape(x_test, (x_test_shape[0], x_test_shape[1] * x_test_shape[2]))
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1] * x_train.shape[2]))
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1] * x_test.shape[2]))
 
 # converting the pixel values from 0 to 1
 x_train = x_train / 255
